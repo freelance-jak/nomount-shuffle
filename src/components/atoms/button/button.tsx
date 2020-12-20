e3cb34683254d28ab35df0c4d8a3c872106d0ea4
aya <ayaaa1027@gmail.com>
2020-12-20 10:38:55 +0900

    Button,Form追加

---
+++ src/components/atoms/button/button.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  name: string;
+  handleonclick: () => void;
+}
+
+export const Button = (props: Props) => {
+  const handleonclick = () => {
+    props.handleonclick();
+  };
+
+  return (
+    <button onClick={handleonclick} class="w-20 h-9 border-black border-2">
+      {props.name}
+    </button>
+  );
+};
---
+++ src/components/atoms/form/form.tsx
@@ -0,0 +1,3 @@
+export const Form = () => {
+  return <input type="text" class="w-40 h-9 border-black border-2"></input>;
+};
